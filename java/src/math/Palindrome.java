d8e09f11e8c3ff72df6948bc51b8a0ff2467ebd7
gopaljilab <email@gopaljilab@gmail.com>
2025-08-04 23:01:49 +0530

    Update Palindrome

--- java/src/math/Palindrome.java
+++ java/src/math/Palindrome.java
@@ -1,5 +1,7 @@
 package math;
 
+import java.util.Scanner;
+
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
@@ -16,9 +18,21 @@ class Solution {
     }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         Solution solution = new Solution();
-        int number=0;
-        boolean result = solution.isPalindrome(number);
-        System.out.println(result);
+        System.out.println("Check Palindrome Or not");
+        System.out.println("Number (Which You Want to Check) :");
+        int number1=121;
+        System.out.println("Number : " + number1);
+        boolean result1 = solution.isPalindrome(number1);
+        System.out.println(result1);
+        int number2=-121;
+        System.out.println("Number : " + number2);
+        boolean result2 = solution.isPalindrome(number2);
+        System.out.println(result2);
+        int number3=10;
+        System.out.println("Number : " + number3);
+        boolean result3 = solution.isPalindrome(number3);
+        System.out.println(result3);
     }
 }
