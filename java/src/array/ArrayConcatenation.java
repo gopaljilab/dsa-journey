025cdcc92d36d5f90334dc97b25d810958d305cb
Gopal Gupta <gopaljilab@gmail.com>
2025-08-08 15:42:06 +0530

    ArrayConcatenation

--- java/src/array/ArrayConcatenation.java
+++ java/src/array/ArrayConcatenation.java
@@ -5,9 +5,16 @@ import java.util.Arrays;
 public class ArrayConcatenation {
     public static void main(String[] args) {
         int[] nums = {1,2,3};
+        int[] nums2 = {1,2,3,4,5};
         System.out.println("Input : " + Arrays.toString(nums));
         int[] ans = getConcatenation(nums);
         System.out.println("Output : " + Arrays.toString(ans));
+        System.out.println();
+
+        System.out.println("Input : " + Arrays.toString(nums2));
+        int[] ans2 = getConcatenation(nums2);
+        System.out.println("Output : " + Arrays.toString(ans2));
+        System.out.println();
     }
 
     public static int[] getConcatenation(int[] nums) {
