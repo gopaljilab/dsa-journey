7558abbab509f4eaca46218fb8c00921cab7ac42
Gopal Gupta <gopaljilab@gmail.com>
2025-08-11 23:45:27 +0530

    Insertion Sort

---
+++ java/src/array/InsertionSort.java
@@ -0,0 +1,34 @@
+package array;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr= {12,11,13,5,6};
+        System.out.print("Unsorted : ");
+        for (int i : arr){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.print("Sorted : ");
+        int[] sortedArray = inertionSort(arr);
+        for (int i : arr){
+            System.out.print(i+ " ");
+        }
+    }
+
+    public static int[] inertionSort(int[] arr){
+        int key,j;
+
+        for (int i=1;i< arr.length;i++){
+            key = arr[i];
+            j = i-1;
+            while ((j >= 0) && (arr[j] > key)){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+
+        return arr;
+    }
+}
+
