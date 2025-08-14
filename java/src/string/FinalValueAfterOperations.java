5401d5f2d6a554f0e29ee2ae30ac4aadcabc2ec5
Gopal Gupta <gopaljilab@gmail.com>
2025-08-14 16:36:56 +0530

    FinalValueAfterOperations

--- java/src/string/FinalValueAfterOperations.java
+++ java/src/string/FinalValueAfterOperations.java
@@ -1,4 +1,23 @@
 package string;
 
-public class finalValueAfterOperations {
+public class FinalValueAfterOperations {
+    public static void main(String[] args) {
+        String[] operations = {"--X", "++X", "X--"};
+        int result = finalValue(operations);
+
+        System.out.print("Input : ");
+        for (int i = 0; i < operations.length; i++) {
+            System.out.print(operations[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Output : " + result);
+    }
+
+    public static int finalValue(String[] operations) {
+        int result = 0;
+        for (String op : operations) {
+            result += (op.equals("--X") || op.equals("X--")) ? -1 : 1;
+        }
+        return result;
+    }
 }
