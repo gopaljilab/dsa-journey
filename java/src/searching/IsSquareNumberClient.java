43f22d979e5e1be240b38a4f087efac44829da33
Gopal Gupta <gopaljilab@gmail.com>
2025-08-19 08:11:24 +0530

    IsSquareNumber

--- java/src/searching/IsSquareNumberClient.java
+++ java/src/searching/IsSquareNumberClient.java
@@ -1,12 +1,13 @@
 package searching;
 
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class IsSquareNumberClient {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the Number : ");
-        Math.sqrt(16);
         int num = input.nextInt();
         IsSquareNumber square = new IsSquareNumber();
         boolean isSquare = IsSquareNumber.isSquareNum(num);
