132718804460aaca2053789350e8cf46084a1940
Gopal Gupta <gopaljilab@gmail.com>
2025-08-19 07:57:33 +0530

    Add two Binary

--- java/src/string/AddBinary.java
+++ java/src/string/AddBinary.java
@@ -1,4 +1,34 @@
 package string;
 
 public class AddBinary {
+    public static void main(String[] args) {
+        String num1 = "11";
+        String num2 = "1";
+        System.out.println("Output : ");
+        String result = add(num1,num2);
+        System.out.println(result);
+    }
+    public static String add(String num1, String num2){
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length()-1;
+        int j = num2.length()-1;
+        int carry = 0;
+        int sum;
+        String result = "";
+        while (i >= 0 || j >= 0 || carry>0){
+            sum = carry;
+            if (i >=0 ){
+                sum = sum + (num1.charAt(i)-'0');
+                i--;
+            }
+            if (j >=0 ){
+                sum = sum + (num2.charAt(j)-'0');
+                j--;
+            }
+            sb.append(sum%2);
+            carry = sum/2;
+        }
+
+        return sb.reverse().toString();
+    }
 }
