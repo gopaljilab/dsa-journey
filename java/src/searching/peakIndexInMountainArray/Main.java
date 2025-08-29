af65bdd864b2c3910e0bedbc0845c67cd20a8c98
Gopal Gupta <gopaljilab@gmail.com>
2025-08-29 07:07:31 +0530

    peakIndexInMountainArray

--- java/src/searching/peakIndexInMountainArray/Main.java
+++ java/src/searching/peakIndexInMountainArray/Main.java
@@ -1,4 +1,12 @@
 package searching.peakIndexInMountainArray;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,3,2,1,0};
+        System.out.println("Input : " + Arrays.toString(arr));
+        int peakIndex = Solution.isPeakIndex(arr);
+        System.out.println("Output : " + peakIndex);
+    }
 }
