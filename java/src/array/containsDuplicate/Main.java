ebfe6ecc41a018d46e48f2442845c3b6560004b6
Gopal Gupta <gopaljilab@gmail.com>
2025-08-29 07:10:02 +0530

    Contains Duplicate

--- java/src/array/containsDuplicate/Main.java
+++ java/src/array/containsDuplicate/Main.java
@@ -1,4 +1,13 @@
 package array.containsDuplicate;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(Arrays.toString(nums));
+        Solution solution = new Solution();
+        boolean isContains = solution.containsDuplicate(nums);
+        System.out.println(isContains);
+    }
 }
--- java/src/array/containsDuplicate/Solution.java
+++ java/src/array/containsDuplicate/Solution.java
@@ -1,4 +1,17 @@
 package array.containsDuplicate;
 
+import java.util.Arrays;
+
 public class Solution {
+    public boolean containsDuplicate(int[] nums){
+        int n = nums.length;
+        Arrays.sort(nums);
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
