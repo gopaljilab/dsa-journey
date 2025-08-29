adcb6d573deffc302e5075336370f569a6b71de5
Gopal Gupta <gopaljilab@gmail.com>
2025-08-29 07:08:44 +0530

    Transpose Of Matrix

--- java/src/searching/transposeMatrix/Main.java
+++ java/src/searching/transposeMatrix/Main.java
@@ -1,4 +1,17 @@
 package searching.transposeMatrix;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,6}};
+        int[][] newMatrix = Solution.transpose(matrix);
+
+        for (int i = 0; i < newMatrix.length ; i++) {
+            for (int j = 0; j < newMatrix[i].length; j++) {
+                System.out.print(newMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- java/src/searching/transposeMatrix/Solution.java
+++ java/src/searching/transposeMatrix/Solution.java
@@ -1,4 +1,15 @@
 package searching.transposeMatrix;
 
 public class Solution {
+    public static int[][] transpose(int[][] matrix){
+        int rows = matrix.length;
+        int column = matrix[0].length;
+        int [][] ans =new int [column][rows];
+        for (int i = 0; i < column; i++) {
+            for (int j = 0; j < rows; j++) {
+                ans[i][j] = matrix[j][i];
+            }
+        }
+        return ans;
+    }
 }
