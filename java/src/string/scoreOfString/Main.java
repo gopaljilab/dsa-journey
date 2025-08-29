b29c08fc0b34fdd470e93cf172c5565bcfc0fe74
Gopal Gupta <gopaljilab@gmail.com>
2025-08-29 07:10:48 +0530

    Score of String

--- java/src/string/scoreOfString/Main.java
+++ java/src/string/scoreOfString/Main.java
@@ -1,4 +1,12 @@
 package string.scoreOfString;
 
 public class Main {
+
+    public static void main(String[] args) {
+        String word = "hello";
+        System.out.println("Input : " + word);
+        Solution solution = new Solution();
+        int result = solution.scoreOfString(word);
+        System.out.println("Output : " + result);
+    }
 }
--- java/src/string/scoreOfString/Solution.java
+++ java/src/string/scoreOfString/Solution.java
@@ -1,4 +1,15 @@
 package string.scoreOfString;
 
 public class Solution {
+    public int scoreOfString(String s){
+        int score = 0, sum = 0;
+        for (int i = 0; i < s.length()-1; i++) {
+            sum = ((int)s.charAt(i) - (int) (s.charAt(i+1)));
+            if (sum < 0){
+                sum = -sum;
+            }
+            score += sum;
+        }
+        return score;
+    }
 }
