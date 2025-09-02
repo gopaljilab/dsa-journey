9a008641d977cf00fc9570088e397f6c76d9031e
Gopal Gupta <gopaljilab@gmail.com>
2025-09-02 21:09:08 +0530

    Target Indices

--- java/src/searching/targetIndices/Main.java
+++ java/src/searching/targetIndices/Main.java
@@ -1,4 +1,15 @@
 package searching.targetIndices;
 
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,2,5,2,3};
+        int target = 2;
+
+        SolutionTargetIndex solutionTargetIndex = new SolutionTargetIndex();
+        List<Integer> result = solutionTargetIndex.targetIndices(nums,target);
+        System.out.println(result);
+
+    }
 }
--- java/src/searching/targetIndices/SolutionTargetIndex.java
+++ java/src/searching/targetIndices/SolutionTargetIndex.java
@@ -1,4 +1,34 @@
 package searching.targetIndices;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class SolutionTargetIndex {
+    public List<Integer> targetIndices(int[] nums, int target) {
+        List<Integer> newNums = new ArrayList<>();
+        Arrays.sort(nums);
+        /*for (int i = 0; i < nums.length; i++) {
+            if(nums[i] == target ){
+                newNums.add(i);
+            }
+        }*/
+
+        int first = 0;
+        int last = nums.length-1;
+        while (first < last){
+            int mid = (first + last)/2;
+            if (nums[mid] == target){
+                newNums.add(mid);
+            }
+            if (nums[mid] < target){
+                first = mid + 1;
+            }else {
+                last = mid - 1;
+            }
+        }
+
+        return newNums;
+    }
 }
