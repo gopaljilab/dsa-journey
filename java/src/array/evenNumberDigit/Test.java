bdf047fc662d443e068e32f4ea69a85b39d81dd8
Gopal Gupta <gopaljilab@gmail.com>
2025-09-02 21:14:09 +0530

    Even Number Digit

--- java/src/array/evenNumberDigit/Test.java
+++ java/src/array/evenNumberDigit/Test.java
@@ -1,4 +1,12 @@
 package array.evenNumberDigit;
 
+import java.util.Arrays;
+
 public class Test {
+    public static void main(String[] args) {
+        int nums[] = {555,901,482,1771,23,12,46,987,43257,2,1,7890,2};
+        System.out.println("Input : " + Arrays.toString(nums));
+        Solution solution = new Solution();
+        System.out.println("Output : " + solution.findNumbers(nums));
+    }
 }
