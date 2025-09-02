f0c07616a7196e898f4a397d51061dd7c8b8021f
Gopal Gupta <gopaljilab@gmail.com>
2025-09-02 21:14:46 +0530

    Replace Elements

--- java/src/array/replaceElements/Solution.java
+++ java/src/array/replaceElements/Solution.java
@@ -1,4 +1,22 @@
 package array.replaceElements;
 
+import java.util.Arrays;
+
 public class Solution {
+    public static int[] replaceElement(int[] arr){
+        int arrLength = arr.length-1;
+        for (int i = 0; i <= arrLength; i++) {
+            int maxValue = -1;
+            for (int j = i + 1; j <= arrLength; j++) {
+                /*if (maxValue < arr[j]){
+                    maxValue = arr[j];
+                }
+                arr[i] = maxValue;*/
+
+                maxValue = Math.max(maxValue,arr[j]);
+            }
+        }
+        arr[arrLength-1] = -1;
+        return arr;
+    }
 }
