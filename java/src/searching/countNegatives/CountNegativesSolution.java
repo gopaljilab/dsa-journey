075e0cc076ec431fc4bd1ec5e8577b84afb4c40c
Gopal Gupta <gopaljilab@gmail.com>
2025-09-02 21:06:12 +0530

    Count Negatives

--- java/src/searching/countNegatives/CountNegativesSolution.java
+++ java/src/searching/countNegatives/CountNegativesSolution.java
@@ -1,4 +1,26 @@
 package searching.countNegatives;
 
-public class CountNegatives {
+public class CountNegativesSolution {
+    public int countNegatives(int[][] grid) {
+        int gridLength = grid.length;
+        int countNegative = 0;
+        for (int i = 0; i < grid.length; i++) {
+            int[] subArray = grid[i];
+            for (int j = 0; j < grid[i].length; j++) {
+                if (subArray[j] < 0) {
+                    countNegative += (subArray.length - countNegative);
+                    break;
+                }
+            }
+            // int left = 0;
+            // int right = subArray.length;
+
+            // while(left<right){
+            //     int mid = left + (right - left);
+
+            //     if(subArray[mid] = -1)
+            // }
+        }
+        return countNegative;
+    }
 }
--- java/src/searching/countNegatives/Main.java
+++ java/src/searching/countNegatives/Main.java
@@ -1,4 +1,10 @@
 package searching.countNegatives;
 
 public class Main {
+    public static void main(String[] args) {
+        int[][] grid = {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+        CountNegativesSolution countNegativesSolution = new CountNegativesSolution();
+        int countNegatives = countNegativesSolution.countNegatives(grid);
+        System.out.println(countNegatives);
+    }
 }
