75d1c5ea97a733e429ebf7b668b07a591870a7e5
Gopal Gupta <gopaljilab@gmail.com>
2025-09-02 21:04:29 +0530

    Two Sum

--- java/src/array/twoSum/Main.java
+++ java/src/array/twoSum/Main.java
@@ -1,4 +1,16 @@
 package array.twoSum;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {2,7,3,9};
+        int target = 9;
+        TwoSumProblem solution = new TwoSumProblem();
+        int[] result = solution.twoSum(nums, target);
+
+        if (result.length == 2) {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No pair found.");
+        }
+    }
 }
--- java/src/array/twoSum/TwoSumProblem.java
+++ java/src/array/twoSum/TwoSumProblem.java
@@ -1,6 +1,4 @@
-package array;
-
-import java.util.Scanner;
+package array.twoSum;
 
 public class TwoSumProblem {
 
@@ -13,32 +11,5 @@ public class TwoSumProblem {
             }
         }
         return new int[] {};
-    }
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Enter number of elements: ");
-        int n = input.nextInt();
-
-        int[] nums = new int[n];
-        System.out.println("Enter " + n + " numbers:");
-        for (int i = 0; i < n; i++) {
-            nums[i] = input.nextInt();
-        }
-
-        System.out.print("Enter target: ");
-        int target = input.nextInt();
-
-        TwoSumProblem solution = new TwoSumProblem();
-        int[] result = solution.twoSum(nums, target);
-
-        if (result.length == 2) {
-            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
-        } else {
-            System.out.println("No pair found.");
-        }
-
-        input.close();
     }
 }
