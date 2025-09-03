deb5f11c888f99c7427122ab9406dc264bf786bf
Gopal Gupta <gopaljilab@gmail.com>
2025-09-03 08:13:02 +0530

    Refactor Contains Duplicate

--- java/src/array/containsDuplicate/Solution.java
+++ java/src/array/containsDuplicate/Solution.java
@@ -1,9 +1,11 @@
 package array.containsDuplicate;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Solution {
-    public boolean containsDuplicate(int[] nums){
+    /** public boolean containsDuplicate(int[] nums){
         int n = nums.length;
         Arrays.sort(nums);
 
@@ -11,6 +13,14 @@ public class Solution {
             if (nums[i] == nums[i - 1]) {
                 return true;
             }
+        }
+        return false;
+     */
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) return true;
+            seen.add(num);
         }
         return false;
     }
