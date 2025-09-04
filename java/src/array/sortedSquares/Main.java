a53dc1b811b0ac5d57704a60239c557c76ab4ac6
Gopal Gupta <gopaljilab@gmail.com>
2025-09-04 06:31:46 +0530

    Sorted Squares

--- java/src/array/sortedSquares/Main.java
+++ java/src/array/sortedSquares/Main.java
@@ -1,4 +1,13 @@
 package array.sortedSquares;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-4,-1,0,3,10};
+        SortedSquaresSol sol = new SortedSquaresSol();
+        int[] result = sol.sortedSquares(nums);
+        System.out.println(Arrays.toString(result));
+        System.out.println(Math.abs(nums[0]));
+    }
 }
--- java/src/array/sortedSquares/SortedSquaresSol.java
+++ java/src/array/sortedSquares/SortedSquaresSol.java
@@ -1,4 +1,31 @@
 package array.sortedSquares;
 
-public class SortedSquares {
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Map;
+
+public class SortedSquaresSol {
+    public int[] sortedSquares(int[] nums){
+        int numsLength = nums.length;
+        int[] newNums = new int[numsLength];
+        /*int square = 0;
+        for (int i = 0; i < numsLength; i++) {
+            square = nums[i] * nums[i];
+            newNums[i] = square;
+        }
+        Arrays.sort(newNums);*/
+
+        int left = 0;
+        int right = numsLength - 1;
+        for (int i = right; i >= 0; i--) {
+            if (Math.abs(nums[left]) > Math.abs(nums[right]) ){
+                newNums[i] = nums[left] * nums[left];
+                left++;
+            }else {
+                newNums[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        return newNums;
+    }
 }
