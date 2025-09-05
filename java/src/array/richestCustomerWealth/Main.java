a17babb236df9d6d9f8dfa6b0554a94a0a667c8e
Gopal Gupta <gopaljilab@gmail.com>
2025-09-05 19:42:21 +0530

    Richest Cumstomer Wealth

--- java/src/array/richestCustomerWealth/Main.java
+++ java/src/array/richestCustomerWealth/Main.java
@@ -1,4 +1,13 @@
 package array.richestCustomerWealth;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[][] customerWealth = {{1,2,3},{3,2,1}};
+        System.out.println("Input : " + Arrays.deepToString(customerWealth));
+        RIchestCustomerWealthSol sol = new RIchestCustomerWealthSol();
+        int result = sol.maximumWealth(customerWealth);
+        System.out.println("Output : " + result);
+    }
 }
--- java/src/array/richestCustomerWealth/RIchestCustomerWealthSol.java
+++ java/src/array/richestCustomerWealth/RIchestCustomerWealthSol.java
@@ -1,4 +1,20 @@
 package array.richestCustomerWealth;
 
 public class RIchestCustomerWealthSol {
+    public int maximumWealth(int[][] accounts){
+        int accountSize = accounts.length;
+        int wealthSize = accounts[0].length;
+        int maxWealth = 0;
+        for (int i = 0; i < accountSize; i++) {
+            int tempMaxWealth = 0;
+            for (int j = 0; j < wealthSize; j++) {
+                tempMaxWealth += accounts[i][j];
+            }
+            if (tempMaxWealth > maxWealth){
+                maxWealth = tempMaxWealth;
+            }
+        }
+
+        return maxWealth;
+    }
 }
