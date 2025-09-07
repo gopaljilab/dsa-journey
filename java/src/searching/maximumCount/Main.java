fec2709a29b84e5e685d6cb92ba681880b20340c
Gopal Gupta <gopaljilab@gmail.com>
2025-09-07 06:07:19 +0530

    Maximum Count Positive or Negative

--- java/src/searching/maximumCount/Main.java
+++ java/src/searching/maximumCount/Main.java
@@ -1,4 +1,13 @@
 package searching.maximumCount;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {-3, -2, -1, 0, 0, 2, 4, 5, 6};
+        System.out.println("Input : " + Arrays.toString(nums));
+        MaximumCountSol sol = new MaximumCountSol();
+        int result = sol.maximumCount(nums);
+        System.out.println("Output : " + result);
+    }
 }
--- java/src/searching/maximumCount/MaximumCountSol.java
+++ java/src/searching/maximumCount/MaximumCountSol.java
@@ -1,4 +1,52 @@
 package searching.maximumCount;
 
 public class MaximumCountSol {
+    // Optimised Binary Search
+
+    public int maximumCount(int[] nums){
+        int length = nums.length;
+
+        int firstZero = lowerBound(nums, 0);
+        int firstPositive = lowerBound(nums, 1);
+
+        int negCount = firstZero;
+        int posCount = length - firstPositive;
+
+        return Math.max(negCount, posCount);
+    }
+
+    private int lowerBound(int[] nums, int target){
+        int low = 0;
+        int high = nums.length;
+
+        while (low < high){
+            int mid = (low + high) / 2;
+
+            if (nums[mid] < target){
+                low = mid + 1;
+            }else {
+                high = mid;
+            }
+
+        }
+        return low;
+    }
+
+
+    /** Brute-Force
+     *
+     *  public int maximumCount(int[] nums){
+        int countMax = 0;
+        int countMin = 0;
+        for (int num : nums) {
+            if (num > 0) {
+                countMax++;
+            } else if (num == 0) {
+                continue;
+            } else {
+                countMin++;
+            }
+        }
+        return Math.max(countMax, countMin);
+     */
 }
