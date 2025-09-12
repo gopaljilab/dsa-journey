a7f6f4842241f8d216e4a51c41b3a712741793f3
Gopal Gupta <gopaljilab@gmail.com>
2025-09-12 23:43:48 +0530

    FizzBuzz

--- java/src/math/fizzBuzz/Main.java
+++ java/src/math/fizzBuzz/Main.java
@@ -1,4 +1,13 @@
 package math.fizzBuzz;
 
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        int n = 15;
+        System.out.println("Number : " + n);
+        FizzBuzzSol sol = new FizzBuzzSol();
+        List<String> list = sol.getList(n);
+        System.out.println(list);
+    }
 }
