bc0c4b1e6afc20b3347ced306bc19552e4b880eb
Gopal Gupta <gopaljilab@gmail.com>
2025-09-13 06:42:05 +0530

    Best time to buy or sell stock

---
+++ java/src/bestTimeBuyOrSellStock/BestProfit.java
@@ -0,0 +1,39 @@
+package bestTimeBuyOrSellStock;
+
+import java.util.Arrays;
+
+public class BestProfit {
+    public int bestProfit(int[] prices){
+        /*Arrays.sort(prices);
+        int buy = prices[0];
+        int sell = prices[prices.length-1];
+        int profit = sell - buy;
+        return profit;*/
+
+
+        /** int maxProfit = 0;
+        int buy = 0, sell = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            buy = prices[i];
+            for (int j = i + 1; j < prices.length; j++) {
+                sell = prices[j];
+                if ((sell - buy) > maxProfit){
+                    maxProfit = sell - buy;
+                }
+            }
+        }
+        return maxProfit; */
+        int maxProfit = 0;
+        int minBuy = Integer.MAX_VALUE;
+
+        for (int sell : prices){
+            if (sell < minBuy){
+                minBuy = sell;
+            } else if (sell - minBuy > maxProfit) {
+                maxProfit = sell - minBuy;
+            }
+        }
+        return maxProfit;
+    }
+}
---
+++ java/src/bestTimeBuyOrSellStock/Main.java
@@ -0,0 +1,13 @@
+package bestTimeBuyOrSellStock;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println(Arrays.toString(prices));
+        BestProfit profit = new BestProfit();
+        int maxProfit = profit.bestProfit(prices);
+        System.out.println(maxProfit);
+    }
+}
