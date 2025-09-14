7a23b9b1cb1a4a256de298b6fffddad76ba7ee1f
Gopal Gupta <gopaljilab@gmail.com>
2025-09-14 06:59:56 +0530

    Plus One at last

--- java/src/array/plusOne/Main.java
+++ java/src/array/plusOne/Main.java
@@ -1,4 +1,13 @@
 package array.plusOne;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] digits = {4,3,2,1};
+        System.out.println("Digits : " + Arrays.toString(digits));
+        PlusOneSol sol = new PlusOneSol();
+        int[] newDigits = sol.plusOne(digits);
+        System.out.println(Arrays.toString(newDigits));
+    }
 }
