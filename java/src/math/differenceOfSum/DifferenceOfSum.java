547869b74490f2bc4fb764a94b44fcc1b5e3d609
Gopal Gupta <gopaljilab@gmail.com>
2025-09-14 07:01:01 +0530

    Difference Of Sum

--- java/src/math/differenceOfSum/DifferenceOfSum.java
+++ java/src/math/differenceOfSum/DifferenceOfSum.java
@@ -1,4 +1,18 @@
 package math.differenceOfSum;
 
 public class DifferenceOfSum {
+    public int differenceOfSums(int n, int m) {
+        int size = 0;
+        int sumNotDiv = 0;
+        int sumDiv = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % m != 0) {
+                sumNotDiv += i;
+            } else {
+                sumDiv += i;
+            }
+        }
+
+        return sumNotDiv - sumDiv;
+    }
 }
--- java/src/math/differenceOfSum/Main.java
+++ java/src/math/differenceOfSum/Main.java
@@ -1,4 +1,13 @@
 package math.differenceOfSum;
 
 public class Main {
+    public static void main(String[] args) {
+        int n = 10;
+        int m = 3;
+
+        System.out.println("m : " + m + "," + " n : " + n);
+        DifferenceOfSum sum = new DifferenceOfSum();
+        int result = sum.differenceOfSums(n,m);
+        System.out.println(result);
+    }
 }
