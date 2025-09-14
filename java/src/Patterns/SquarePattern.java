0d4119b4444ebb724b95a50a9a84ca429c8ad220
Gopal Gupta <gopaljilab@gmail.com>
2025-09-14 07:04:18 +0530

    Pattern problems

--- java/src/Patterns/SquarePattern.java
+++ java/src/Patterns/SquarePattern.java
@@ -1,4 +1,17 @@
 package Patterns;
 
+import java.util.Scanner;
+
 public class SquarePattern {
+    public static void main(String[] gopal) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("How many rows or columns you want to print?  ");
+        int n = input.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
