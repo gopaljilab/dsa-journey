e3938e2424d8732582fd579f8d39286d6a9620fb
Gopal Gupta <gopaljilab@gmail.com>
2025-09-14 23:23:58 +0530

    Contains Duplicate

--- java/src/array/containsduplicate/Main.java
+++ java/src/array/containsduplicate/Main.java
@@ -1,4 +1,4 @@
-package array.containsDuplicate;
+package array.containsduplicate;
 
 import java.util.Arrays;
 
--- java/src/array/containsduplicate/Solution.java
+++ java/src/array/containsduplicate/Solution.java
@@ -1,6 +1,5 @@
-package array.containsDuplicate;
+package array.containsduplicate;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
