e7259ef33392e968fdafd44bef7a624878bec639
Gopal Gupta <gopaljilab@gmail.com>
2025-09-17 23:56:09 +0530

    Array Partition

--- java/src/array/arraypairsum/ArrayPairSumSol.java
+++ java/src/array/arraypairsum/ArrayPairSumSol.java
@@ -1,4 +1,16 @@
 package array.arraypairsum;
 
-public class ArrayPAirSum {
+import java.util.Arrays;
+
+public class ArrayPairSumSol {
+    public int arrayPairSum(int[] nums){
+        Arrays.sort(nums);
+        int sum = 0;
+        int i = nums.length-1;
+        while (i > 0){
+            sum += Math.min(nums[i], nums[i-1]);
+            i-=2;
+        }
+        return sum;
+    }
 }
--- java/src/array/arraypairsum/Main.java
+++ java/src/array/arraypairsum/Main.java
@@ -1,4 +1,13 @@
 package array.arraypairsum;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {6,2,6,5,1,2};
+        System.out.println("" + Arrays.toString(nums));
+        ArrayPairSumSol sol = new ArrayPairSumSol();
+        int result = sol.arrayPairSum(nums);
+        System.out.println(result);
+    }
 }
