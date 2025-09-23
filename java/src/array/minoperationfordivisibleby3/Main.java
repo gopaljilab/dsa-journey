e0d43515137681cd8cd1f7cd0a9b307c3afadb5c
Gopal Gupta <gopaljilab@gmail.com>
2025-09-23 22:25:02 +0530

    Min operation for divisible by 3

--- java/src/array/minoperationfordivisibleby3/Main.java
+++ java/src/array/minoperationfordivisibleby3/Main.java
@@ -1,4 +1,14 @@
 package array.minoperationfordivisibleby3;
 
+import java.util.Arrays;
+
 public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println("Nums: " + Arrays.toString(nums));
+
+        MinimumOperations minimumOperations = new MinimumOperations();
+        int minOperations = minimumOperations.minimumOperations(nums);
+        System.out.println("Output : " + minOperations);
+    }
 }
--- java/src/array/minoperationfordivisibleby3/MinimumOperations.java
+++ java/src/array/minoperationfordivisibleby3/MinimumOperations.java
@@ -1,4 +1,28 @@
 package array.minoperationfordivisibleby3;
 
 public class MinimumOperations {
+    public int minimumOperations(int[] nums) {
+        int minOperations = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int minOperationForAElement = 0;
+            int operationsByDecreasing = 0;
+            int operationsByIncreasing = 0;
+            if (!(nums[i] % 3 == 0)) {
+                int elementIncreasing = nums[i];
+                while (!(elementIncreasing % 3 == 0)) {
+                    elementIncreasing += 1;
+                    operationsByIncreasing += 1;
+                }
+
+                int elementDecreasing = nums[i];
+                while (elementDecreasing % 3 != 0) {
+                    elementDecreasing -= 1;
+                    operationsByDecreasing += 1;
+                }
+            }
+            minOperationForAElement = Math.min(operationsByIncreasing, operationsByDecreasing);
+            minOperations += minOperationForAElement;
+        }
+        return minOperations;
+    }
 }
