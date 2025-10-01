a2fcb027315dbac3452e7b7c803e3e0a55560771
Gopal Gupta <gopaljilab@gmail.com>
2025-10-01 05:55:20 +0530

    addString

--- java/src/string/addstring/AddStringMain.java
+++ java/src/string/addstring/AddStringMain.java
@@ -1,4 +1,12 @@
 package string.addstring;
 
 public class AddStringMain {
+    public static void main(String[] args) {
+        String num1 = "3876620623801494171";
+        String num2 = "6529364523802684779";
+        System.out.println("Input : num1 = " + num1 + " , num2 = " + num2);
+        AddStringSol sol = new AddStringSol();
+        String result = sol.addString(num1, num2);
+        System.out.println("Output : " + result);
+    }
 }
--- java/src/string/addstring/AddStringSol.java
+++ java/src/string/addstring/AddStringSol.java
@@ -1,4 +1,17 @@
 package string.addstring;
 
+import java.math.BigInteger;
+
 public class AddStringSol {
+    public String addString(String num1, String num2) {
+        BigInteger bigNum1 = new BigInteger(num1);
+        BigInteger bigNum2 = new BigInteger(num2);
+
+        BigInteger sum = bigNum1.add(bigNum2);
+        String result2 = sum.toString();
+
+        System.out.println(result2);
+
+        return result2;
+    }
 }
