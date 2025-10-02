a76fe83b5fd6d8f68816b74258a89c6f4509155c
Gopal Gupta <gopaljilab@gmail.com>
2025-10-02 23:52:53 +0530

    get Final State

--- java/src/array/getfinalstate/GetFinalStateMain.java
+++ java/src/array/getfinalstate/GetFinalStateMain.java
@@ -1,4 +1,16 @@
 package array.getfinalstate;
 
+import java.util.Arrays;
+
 public class GetFinalStateMain {
+    public static void main(String[] args) {
+        int[] nums = {2,1,3,5,6};
+        int k = 5;
+        int multiplier = 2;
+        System.out.println("Input : nums = " + Arrays.toString(nums)
+                + ", k = " + k + ", multiplier = " + multiplier);
+        GetFinalStateSol sol = new GetFinalStateSol();
+        int[] result = sol.finalState(nums, k, multiplier);
+        System.out.println("Output : " + Arrays.toString(result));
+    }
 }
