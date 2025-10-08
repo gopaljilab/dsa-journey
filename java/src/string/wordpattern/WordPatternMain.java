7a5171cddb85883583df2bc02474dd3a3041a65a
Gopal Gupta <gopaljilab@gmail.com>
2025-10-08 23:48:01 +0530

    Word Pattern Refactor

--- java/src/string/wordpattern/WordPatternMain.java
+++ java/src/string/wordpattern/WordPatternMain.java
@@ -7,6 +7,9 @@ public class WordPatternMain {
         System.out.println("Input : pattern = " + pattern + " ,s = " + s);
         WordPatternSol sol = new WordPatternSol();
         boolean result = sol.wordPattern(pattern, s);
-        System.out.println("Output : " + result);
+        System.out.println("Output : " + result);   // true ,
+                                                   // false for
+                                                   // Input: pattern = "abba", s = "dog cat cat fish"
+                                                    // Output: false
     }
 }
--- java/src/string/wordpattern/WordPatternSol.java
+++ java/src/string/wordpattern/WordPatternSol.java
@@ -1,22 +1,34 @@
 package string.wordpattern;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 class WordPatternSol {
     public boolean wordPattern(String pattern, String s) {
 
-        Map<Character, String> stringMap = new HashMap<>();
-        String[] wordArray = s.split(" ");
-        for (int i = 0; i < pattern.length(); i++) {
-            stringMap.put(pattern.charAt(i), wordArray[i]);
-        }
-        System.out.println(stringMap);
+        Map<Character, String> patternToWordMap = new HashMap<>();
+        Set<String> mappedWords = new HashSet<>();
+
+        String[] words = s.split(" ");
+
+        if (pattern.length() != words.length) return false;
 
         for (int i = 0; i < pattern.length(); i++) {
-            System.out.println((stringMap.get(pattern.charAt(i))));
-            if (!(stringMap.get(pattern.charAt(i)) == wordArray[i])){
-                return false;
+            char currentChar = pattern.charAt(i);
+            String currentWord = words[i];
+
+            if (patternToWordMap.containsKey(currentChar)) {
+                if (!patternToWordMap.get(currentChar).equals(currentWord)) {
+                    return false;
+                }
+            } else {
+                if (mappedWords.contains(currentWord)) {
+                    return false;
+                }
+                patternToWordMap.put(currentChar, currentWord);
+                mappedWords.add(currentWord);
             }
         }
         return true;
