96db32da7e73187e8550f22c650d83255545e8da
Gopal Gupta <gopaljilab@gmail.com>
2025-11-02 00:00:56 +0530

    Solution : Shuffle Array

---
+++ java/src/math/leetcodebank/TotalMoney.java
@@ -0,0 +1,45 @@
+package math.leetcodebank;
+
+import java.util.Scanner;
+
+/**
+ *
+ * int weeks = n / 7; // total weeks
+ * int days = n % 7; // leftover days
+ *
+ * sumWeeks= weeks×(2×28+(weeks)×7)/2;
+ *
+ * sumDays= days[2×(weeks+1)+(days−1)]/2
+ *
+ * uses of summation of upto n numbers in AP(arithmatic progression)
+ */
+
+public class TotalMoney {
+    public static int totalMoney(int n){
+        /*int total = 0;
+        int weeks = 0;
+        while (n > 0){
+            for (int day = 1; day <= 7 && n > 0; day++) {
+                total += weeks + day;
+                n--;
+            }
+            weeks++;
+        }
+        return total;*/
+
+        int weeks = n / 7; // total weeks
+        int days = n % 7; // leftover days
+        int totalWeeks = weeks * (49 + 7 * weeks) / 2;  // total from all full weeks
+        int totalDays = days * (2 * (weeks + 1) + (days - 1)) / 2; // leftover days
+
+        return totalWeeks + totalDays;
+    }
+
+    public static void main(String[] args) {
+        int n = 20;
+        System.out.println(n);
+        int total = totalMoney(n);
+        System.out.println(total);
+        System.out.println(args.length);
+    }
+}
