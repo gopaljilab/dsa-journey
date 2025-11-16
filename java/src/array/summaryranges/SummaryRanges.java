a3efa1a517736500b8ad184da7f67a673511a45f
Gopal Gupta <gopaljilab@gmail.com>
2025-11-16 22:08:54 +0530

    Solution #228 : Summary Ranges

---
+++ java/src/array/summaryranges/SummaryRanges.java
@@ -0,0 +1,32 @@
+package array.summaryranges;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SummaryRanges {
+    public static List<String> summaryRanges(int[] nums){
+        List<String> result = new ArrayList<>();
+        if (nums.length == 0) {
+            return result;
+        }
+
+        int i = 0;
+        while (i<nums.length){
+            int start = nums[i];
+            int j = i;
+            // Expand the range as long as elements are consecutive
+            while (j + 1 < nums.length && nums[j + 1] == nums[j] + 1){
+                j++;
+            }
+
+            if (nums[j] == start){
+                result.add(String.valueOf(start));
+            }else {
+                result.add(start + "->" + nums[j]);
+            }
+            // Move to the next potential start of a range
+            i = j + 1;
+        }
+        return result;
+    }
+}
